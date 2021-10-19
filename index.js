c55b8eeb6e4f3e0cff6b353756bf682dc579819d
Younes <yuyu1403@gmail.com>
2021-10-19 17:20:23 +0200

    final bJ game try

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let player = {
+    name: "Jojo",
+    chips: 3000
+}
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
 
-// Make this function return a random number between 1 and 13
+playerEl.textContent = player.name + ": $" + player.chips
+
 function getRandomCard() {
-    let randomChoice = Math.floor(Math.random()*13)+1
-    if (randomChoice > 10) {
+    let randomNumber = Math.floor( Math.random()*13 ) + 1
+    if (randomNumber > 10) {
         return 10
-    } else if (randomChoice === 1){
+    } else if (randomNumber === 1) {
         return 11
     } else {
-        return randomChoice
+        return randomNumber
     }
 }
 
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
 
@@ -46,8 +55,10 @@ function renderGame() {
 
 
 function newCard() {
-    let card = getRandomCard()
-    sum += card
-    cards.push(card)
-    renderGame()
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()        
+    }
 }
